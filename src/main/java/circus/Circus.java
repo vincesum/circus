aa48fb6181339dc579cf88bcf772f762491f330a
Akshay Narayan <okkhoy@gmail.com>
2025-02-07 12:39:59 +0800

    Import classes from circus.animal package in Circus and Trainer classes

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -1,5 +1,9 @@
 package circus;
 
+import circus.animal.Animal;
+import circus.animal.Duck;
+import circus.animal.Parrot;
+
 public class Circus {
     private static Animal[] animals = {
             new Duck(),
